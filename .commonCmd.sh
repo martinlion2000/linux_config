b1c2bfd2f862e71ebca24a5e6014458b1c425be4
Martin Tang <martin.tang@ericsson.com>
2013-12-06 17:59:47 +0800

    monitor 2 more files: .git-completion.sh, .commonCmd.sh

---
+++ .commonCmd.sh
@@ -0,0 +1,20 @@
+NORMAL=$(tput sgr0)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+RED=$(tput setaf 1)
+
+function red() {
+echo -e "$RED$*$NORMAL"
+}
+
+function green() {
+ 
+echo -e "$GREEN$*$NORMAL"
+ 
+}
+
+function yellow() {
+echo -e "$YELLOW$*$NORMAL"
+}
+
+function debug() { [ "$DEBUG" ] && echo ">>> $*"; }
